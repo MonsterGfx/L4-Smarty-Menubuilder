e4cb4412610182eed1db27f0eacd25343e0c9279
Dave Thomas <dthomas@starkartthenala.com>
2013-07-16 08:54:56 -0700

    added Menubuilder class

---
+++ src/Monstergfx/Menubuilder/Menubuilder.php
@@ -0,0 +1,28 @@
+<?php namespace Monstergfx\Menubuilder;
+
+class Menubuilder {
+
+
+
+
+	/**
+	 * Build the menu array structure based on the menus configuration file
+	 * and the currently logged in user.
+	 *
+	 * @return array
+	 */
+	public function build()
+	{
+
+echo "howdy!"; die;
+	}
+
+
+
+
+
+
+
+
+
+}
