8b15b73ad15e8a5a89f8cf533aad9319365fb821
Dave Thomas <dthomas@starkartthenala.com>
2013-07-16 08:55:05 -0700

    added Menubuilder facade

---
+++ src/Monstergfx/Menubuilder/Facades/Menubuilder.php
@@ -0,0 +1,14 @@
+<?php namespace Monstergfx\Menubuilder\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Menubuilder extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'menubuilder'; }
+
+}
