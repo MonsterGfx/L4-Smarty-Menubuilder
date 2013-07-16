0b22967d0dab01a4b3ef0c8125f001585b24265f
Dave Thomas <info@monstergfx.com>
2013-07-16 11:12:10 -0700

    added injection of the app container to the menubuilder class

--- src/Monstergfx/Menubuilder/Menubuilder.php
+++ src/Monstergfx/Menubuilder/Menubuilder.php
@@ -2,8 +2,22 @@
 
 class Menubuilder {
 
+	/**
+	 * A (private) instance of the app container, initialized via injection
+	 */
+	private $app = null;
 
-
+	/**
+	 * The class constructor
+	 *
+	 * @param Illuminate\Foundation\Application $application
+	 * The application container
+	 */
+	public function __construct(\Illuminate\Foundation\Application $application)
+	{
+		// save the application container instance
+		$this->app = $application;
+	}
 
 	/**
 	 * Build the menu array structure based on the menus configuration file
