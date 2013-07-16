e4558c43c1736cdc8ed39d19a5f7f5e2b9398ab5
Dave Thomas <info@monstergfx.com>
2013-07-16 11:14:41 -0700

    added buildMenuArray method to menubuilder

--- src/Monstergfx/Menubuilder/Menubuilder.php
+++ src/Monstergfx/Menubuilder/Menubuilder.php
@@ -25,18 +25,49 @@ class Menubuilder {
 	 *
 	 * @return array
 	 */
-	public function build()
+	protected function buildMenuArray()
 	{
 
-echo "howdy!"; die;
+		// references to objects that will be needed
+		$sentry = $this->app['sentry'];
+		$config = $this->app['config'];
+
+		// and one more that's useful for development
+		$kint = $this->app['kint'];
+
+		// get the current user
+		$user = $sentry->getUser();
+		// if nobody's logged in, return an empty array
+		if(!$user)
+			return array();
+
+		// get the menu structure from the config
+		$menus = $config->get('menus.menus');
+
+		// step through the menus & items
+		foreach(array_keys($menus) as $m)
+		{
+			$items = $menus[$m]['items'];
+			$permitted_items = array();
+			foreach($items as $i)
+			{
+				if($user->hasAccess('admin') || $user->hasAccess($i['permission']))
+					$permitted_items[] = $i;
+			}
+			$menus[$m]['items'] = $permitted_items;
+		}
+
+		// step through the top level menus
+		foreach(array_keys($menus) as $m)
+		{
+			// if the current menu is empty, remove it
+			if(count($menus[$m]['items'])==0)
+				unset($menus[$m]);
+		}
+
+$kint->dump($menus); die;
+		// return the result
+		return $menus;
 	}
-
-
-
-
-
-
-
-
 
 }
